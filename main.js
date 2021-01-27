aec5568d6fea7af42a029543705b07ae91b5f818
AbhitanSaha <72934030+AbhitanSaha@users.noreply.github.com>
2021-01-27 21:06:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,23 @@ function take_snapshot(){
 }
 console.log('ml5.version',ml5.version);
 classifier= ml5.imageClassier('https://teachablemachine.withgoogle.com/models/OR1LHUS0G/model.json',modelLoaded);
+function  modelLoaded () {
+    console.log("Model Loaded");
+}
+function check() {
+    img =document.getElementById("captured-img");
+    classifier.classify(img, gotResult);
+}
+function gotResult(error,results) {
+    if (error) {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_name").innerHTML=results[0].label;
+        accuracy=results[0].confidence.toFixed(2);
+        console.log(accuracy);
+        accuracy=accuracy*100;
+        document.getElementById("result_accuracy").innerHTML=accuracy+"%";
+    }
+}
